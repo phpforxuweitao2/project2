f1731dc779cb8be2931c9edfb8d3748bcb3049b5
phpforxuweitao <xuweitao1139204319@foxmail.com>
2018-08-26 18:58:50 +0800

    check

--- resources/views/admin/AdminPublic/footerinfo.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="foods">
-    <ul>版权所有@2015</ul>
-    <dl><a href="" title="返回头部" class="am-icon-btn am-icon-arrow-up"></a></dl>
-</div>
--- resources/views/admin/AdminPublic/header.blade.php
+++ resources/views/admin/AdminPublic/header.blade.php
@@ -5,23 +5,16 @@
         <ul class="am-nav am-nav-pills am-topbar-nav admin-header-list">
 
             <li class="am-dropdown tognzhi" data-am-dropdown>
-                <button class="am-btn am-btn-primary am-dropdown-toggle am-btn-xs am-radius am-icon-bell-o" data-am-dropdown-toggle> 消息管理<span class="am-badge am-badge-danger am-round">6</span></button>
+                <button style="padding-right: 14px;" class="am-btn am-btn-primary am-dropdown-toggle am-btn-xs" data-am-dropdown-toggle> 用户名是发 <i class="am-icon-sort-desc"></i>
+                </button>
                 <ul class="am-dropdown-content">
-                    <li class="am-dropdown-header">所有消息都在这里</li>
-                    <li><a href="#">未激活会员 <span class="am-badge am-badge-danger am-round">556</span></a>
-                    </li>
-                    <li><a href="#">未激活代理 <span class="am-badge am-badge-danger am-round">69</span></a>
-                    </li>
-                    <li><a href="#">未处理汇款</a></li>
-                    <li><a href="#">未发放提现</a></li>
-                    <li><a href="#">未发货订单</a></li>
-                    <li><a href="#">低库存产品</a></li>
-                    <li><a href="#">信息反馈</a></li>
+                    <li><a href="#">个人信息</a></li>
+                    <li><a href="#">修改密码</a></li>
+                    <li><a href="#">退出登录</a></li>
                 </ul>
             </li>
 
             <li class="kuanjie">
-
                 <a href="#">会员管理</a>
                 <a href="#">奖金管理</a>
                 <a href="#">订单管理</a>
@@ -32,7 +25,6 @@
 
             <li class="soso">
                 <p>
-
                     <select data-am-selected="{btnWidth: 70, btnSize: 'sm', btnStyle: 'default'}">
                         <option value="b">全部</option>
                         <option value="o">产品</option>
