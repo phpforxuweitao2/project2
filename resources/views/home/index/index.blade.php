b92f4875f2ca72c39402092faaa70723d13824f0
phpforxuweitao <xuweitao1139204319@foxmail.com>
2018-08-31 20:21:06 +0800

    xwt

--- resources/views/home/index/index.blade.php
+++ resources/views/home/index/index.blade.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
  <head>
@@ -371,4 +369,3 @@ window.location = "https://m.riji.cn";
 
  </script>
 </html>
->>>>>>> 307aa881978e57a7c8687e0ee23c15475f1d7cda
